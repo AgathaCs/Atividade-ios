9dec5d50c2aa7adeffef51ec849fbb27ec5da057
kauanty <kauangfx@hotmail.com>
2022-10-11 21:42:17 -0300

    subindo arquivos referente a aula04 de js

---
+++ JavaScript/atividade para casa/estruturas.js
@@ -0,0 +1,14 @@
+// Primeira atividade
+
+const A = 20;
+
+const B = 15;
+
+
+const C = 10;
+
+
+if (A>15 || B>12 || C>5){
+    console.log(`Condição é ${A>15|| B>12 || C>5}`);
+    console.log(A é maior do que 15 ou B é maior)
+}
