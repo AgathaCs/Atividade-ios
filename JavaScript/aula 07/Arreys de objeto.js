8e0da465ccf6239034a72de95fb81c3226b535b3
kauanty <kauangfx@hotmail.com>
2022-10-18 21:49:45 -0300

    subindo arquivo referente a aula07

---
+++ JavaScript/aula 07/Arreys de objeto.js
@@ -0,0 +1,17 @@
+const Alunos = [
+    {
+    Nota: 10,
+    Endereço: 'Rua: Maria Branca, nº55',
+    isCompleted: [aprovado],
+    },
+    {
+    Nota: 7,
+    Endereço: 'Rua: Travessa Gildasio Almeida, nº46',
+    isCompleted: true,
+    },
+    {
+    Nota: 4,
+    Endereço: 'Rua: Jucuruçu',
+    isCompleted: false,
+    },
+    ];
